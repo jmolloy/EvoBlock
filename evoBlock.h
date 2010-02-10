c5b4edde16290211610342f625e191cc3215552b
Mark Goodall <markit@seken.co.uk>
2010-02-10 16:45:06 +0000

    Added more missing dependancies

--- evoBlock.h
+++ evoBlock.h
@@ -1,6 +1,9 @@
 #ifndef __evoBlock_H__
 #define __evoBlock_H__
 
+#include "EvoBlockSim.h"
+#include <stdlib.h>
+
 #define NUM_CELLS 40
 #define NUM_CONFIG_BITS 20
 
@@ -9,5 +12,23 @@ typedef struct
 	Xuint32 cellConfigs[NUM_CELLS];
 	Xuint32 fitness;
 } ArrayConfig;
+
+void randomiseArrayConfig(ArrayConfig *config)
+{
+	Xuint32 cellNum = 0;
+	Xuint32 randNum = 0;
+
+	// Randomise cell configs
+	for (cellNum=0 ; cellNum<NUM_CELLS ; cellNum++)
+	{
+		config->cellConfigs[cellNum] = rand();
+	}
+}
+
+EvoBlockSim ebs();
+
+void EVOBLOCK_writeMask(Xuint32 data) {
+
+}
 
 #endif //__evoBlock_H__
