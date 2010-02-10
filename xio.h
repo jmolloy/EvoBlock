6ff3f8c840b17c06a572cfd6d77d12303c02faa4
Mark Goodall <markit@seken.co.uk>
2010-02-10 16:19:21 +0000

    Added stub replacements

---
+++ xio.h
@@ -0,0 +1,9 @@
+#ifndef __XIO_H__
+#define __XIO_H__
+
+#include <stdio.h>
+
+#define print(x) printf(x)
+#define xil_printf printf
+
+#endif //__XIO_H__
---
+++ xparameters.h
@@ -0,0 +1,7 @@
+#ifndef __XPARAMETERS_H__
+#define __XPARAMETERS_H__
+
+#define Xuint32 (unsigned int)
+#define Xuint8 (unsigned char)
+
+#endif //__XPARAMETERS_H__
