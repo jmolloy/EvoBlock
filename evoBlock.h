52f3d3bb1a801c7414c0b60c39bd119c3c6bf390
Mark Goodall <markit@seken.co.uk>
2010-02-10 18:34:01 +0000

    Fixed silly bugs

--- evoBlock.h
+++ evoBlock.h
@@ -33,13 +33,13 @@ void EVOBLOCK_writeVector(Xuint32 vector) {
 }
 
 void EVOBLOCK_writeTargetRAM(Xuint32 input) {
-	ebs[0].SetTruthTable(hack_vector++, input);
-	ebs[1].SetTruthTable(hack_vector++, input);
-	ebs[2].SetTruthTable(hack_vector++, input);
-	ebs[3].SetTruthTable(hack_vector++, input);
-	ebs[4].SetTruthTable(hack_vector++, input);
-	ebs[5].SetTruthTable(hack_vector++, input);
-	ebs[6].SetTruthTable(hack_vector++, input);
+	ebs[0].SetTruthTable(hack_vector, input);
+	ebs[1].SetTruthTable(hack_vector, input);
+	ebs[2].SetTruthTable(hack_vector, input);
+	ebs[3].SetTruthTable(hack_vector, input);
+	ebs[4].SetTruthTable(hack_vector, input);
+	ebs[5].SetTruthTable(hack_vector, input);
+	ebs[6].SetTruthTable(hack_vector, input);
 	ebs[7].SetTruthTable(hack_vector++, input);
 }
 
@@ -62,14 +62,13 @@ void configureArray(Xuint32 arrayNum, ArrayConfig *config) {
 
 // Arrays 0 - 3
 Xuint32 EVOBLOCK_readMatch0() {
-	Xuint32 result = ebs[0].Calc(hack_vector) + (ebs[1].Calc(hack_vector) << 8) + (ebs[2].Calc(hack_vector) << 16) + (ebs[3].Calc(hack_vector) << 24);
-	hack_vector++;
+	Xuint32 result = ebs[0].Calc(hack_vector) | (ebs[1].Calc(hack_vector) << 8) | (ebs[2].Calc(hack_vector) << 16) | (ebs[3].Calc(hack_vector) << 24);
 	return result;
 }
 
 // Arrays 4-7
 Xuint32 EVOBLOCK_readMatch1() {
-	Xuint32 result = ebs[4].Calc(hack_vector) + (ebs[5].Calc(hack_vector) << 8) + (ebs[6].Calc(hack_vector) << 16) + (ebs[7].Calc(hack_vector) << 24);
+	Xuint32 result = ebs[4].Calc(hack_vector) | (ebs[5].Calc(hack_vector) << 8) | (ebs[6].Calc(hack_vector) << 16) | (ebs[7].Calc(hack_vector) << 24);
 	hack_vector++;
 	return result;
 }
